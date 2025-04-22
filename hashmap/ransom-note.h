ee07fd5aeafa9eacc3e94b8c49a617cc08557f0e
Sarawut Ponsan <phaiza237@gmail.com>
2025-04-22 23:05:27 +0700

    Update ransom-note.h

---
+++ hashmap/ransom-note.h
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+        bool canConstruct(string ransomNote, string magazine) {
+            int count = 0;
+            int count_mamgaize = magazine.length(), count_ransomNote = ransomNote.length();
+            
+            if(count_ransomNote > count_mamgaize) return false;
+
+            for(int i = 0; i < count_ransomNote; i++)
+            {
+                for(int j = 0; j < count_mamgaize; j++)
+                {
+                    if(ransomNote[i] == magazine[j])
+                    {
+                        magazine[j] = '0';
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            if(count == count_ransomNote) return true;
+            return false;
+        }
+    };
