fffc9a35b8a3499bf468e66c137c30ccd7eb75c6
Sarawut Ponsan <phaiza237@gmail.com>
2025-08-29 17:58:15 +0700

    Solve leetcode solution: Contains-Duplicate-II

---
+++ hashmap/Contains-Duplicate-II.h
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i + 1; j < i + k + 1 && j < nums.size(); j++)
+            {
+                if(nums[i] == nums[j]) return true;
+            }
+        }
+
+        return false;
+    }
+};
