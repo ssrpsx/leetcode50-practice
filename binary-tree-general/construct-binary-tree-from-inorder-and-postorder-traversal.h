152da27c2b12498eabf3e4d4e924a2ff690e8914
Sarawut Ponsan <122785047+ssrpsx@users.noreply.github.com>
2025-11-09 17:18:37 +0700

    Update construct-binary-tree-from-inorder-and-postorder-traversal.h

--- binary-tree-general/construct-binary-tree-from-inorder-and-postorder-traversal.h
+++ binary-tree-general/construct-binary-tree-from-inorder-and-postorder-traversal.h
@@ -1,40 +1,40 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
+    #include <vector>
+    #include <unordered_map>
+    using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-};
+    struct TreeNode {
+        int val;
+        TreeNode* left;
+        TreeNode* right;
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    };
 
-class Solution {
-    unordered_map<int,int> inorderMap;
-    int preIndex = 0;
+    class Solution {
+        unordered_map<int,int> inorderMap;
+        int preIndex = 0;
 
-    TreeNode* build(vector<int>& preorder, int inStart, int inEnd) {
-        if (inStart > inEnd) return nullptr;
+        TreeNode* build(vector<int>& preorder, int inStart, int inEnd) {
+            if (inStart > inEnd) return nullptr;
 
-        int rootVal = preorder[preIndex++];
-        TreeNode* root = new TreeNode(rootVal);
+            int rootVal = preorder[preIndex++];
+            TreeNode* root = new TreeNode(rootVal);
 
-        int inIndex = inorderMap[rootVal];
+            int inIndex = inorderMap[rootVal];
 
-        root->left = build(preorder, inStart, inIndex - 1);
-        root->right = build(preorder, inIndex + 1, inEnd);
+            root->left = build(preorder, inStart, inIndex - 1);
+            root->right = build(preorder, inIndex + 1, inEnd);
 
-        return root;
-    }
+            return root;
+        }
 
-public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        inorderMap.clear();
-        preIndex = 0;
+    public:
+        TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+            inorderMap.clear();
+            preIndex = 0;
 
-        for (int i = 0; i < inorder.size(); ++i)
-            inorderMap[inorder[i]] = i;
+            for (int i = 0; i < inorder.size(); ++i)
+                inorderMap[inorder[i]] = i;
 
-        return build(preorder, 0, inorder.size() - 1);
-    }
-};
+            return build(preorder, 0, inorder.size() - 1);
+        }
+    };
