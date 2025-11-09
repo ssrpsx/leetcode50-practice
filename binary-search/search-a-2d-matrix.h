2802f8611f4f2feea13c2579a40971e71e759b7c
Sarawut Ponsan <122785047+ssrpsx@users.noreply.github.com>
2025-11-09 17:12:59 +0700

    Solve problem

---
+++ binary-search/search-a-2d-matrix.h
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) return false;
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / cols][mid % cols];
+
+            if (midValue == target)
+                return true;
+            else if (midValue < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return false;
+    }
+};
